ea79c525afbf3e6e24c9512de4ab2cbd1cdca05a
NaokiM03 <naoki.matsumura@sansan.com>
2021-06-07 09:01:35 +0900

    make ResultParams available

--- src/lib.rs
+++ src/lib.rs
@@ -3,4 +3,4 @@ mod config_ron;
 mod parser;
 mod result_params;
 
-pub use crate::{arg::Arg, config_ron::Config, parser::Parser};
+pub use crate::{arg::Arg, config_ron::Config, parser::Parser, result_params::ResultParams};
